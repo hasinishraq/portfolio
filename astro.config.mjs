687ceafb0d8abc377a59c2e254443ca7b679c96a
hasinishraq <hasinishraq26@gmail.com>
2025-11-13 20:27:42 +0600

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config for upload
 export default defineConfig({
    site: 'https://hasinishraq.github.io',
+   base: '/portfolio',
     vite: {
       plugins: [tailwindcss()],
     },
