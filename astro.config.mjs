a9f2a1de66cd8467955c2d456d172f9be481343a
hasinishraq <hasinishraq26@gmail.com>
2025-11-13 20:23:42 +0600

    based removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config for upload
 export default defineConfig({
    site: 'https://hasinishraq.github.io',
-   base: '/portfolio',
     vite: {
       plugins: [tailwindcss()],
     },
